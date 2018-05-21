fad0157edf6a8e45e5340bcc18d8058e151ea99d
Miller <millera16@debian-9.3.0-template>
2018-05-20 21:22:58 -0700

    Added appropriate failure conditions for the inital arguements. Added the skeleton for timing the search.

--- lab4.c
+++ lab4.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<string.h>
+#include<time.h>
 
 //lab4.c
 //Andrew Miller
@@ -9,19 +10,52 @@
 
 
 
-int main(int argc, char *argv)
+int main(int argc, char *argv[])
 {
 	//Failiure condition for incorrect number of arguements
 	//gives an error message and fails gracefully
 	if (argc != 3)
 	{
 		printf("Error. Improper number of arguements.");
-		printf("Please enter only the search term follwed by the");
-		printf("starting directory\n");
+		printf(" Please enter only the search term follwed by the");
+		printf(" starting directory\n");
+		return 1;
+	}
+	//Failure condition for starting directory not being an absolute path
+	//Checks for first and last character '/'
+	if (argv[2][0] != '/' || argv[2][(int)strlen(argv[2]) - 1] == '/')
+	{
+		printf("Error. Please ensure that the starting directory");
+		printf(" is an absoulte directory\n");
 		return 1;
 	}
 
+	
 
+
+
+	//Starts the search now. Begins the timer with begin
+	clock_t begin = clock();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	//End of the search, get a time for the end to find the runtime
+	clock_t end = clock();
+	double runtime = (double)(end-begin) / CLOCKS_PER_SEC;
+
+	
 
 
 
